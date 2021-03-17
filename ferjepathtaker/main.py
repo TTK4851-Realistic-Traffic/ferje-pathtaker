eb616a13032bc21412f4a93191c1fc9c7c1550ab
Fredrik F. Lindhagen <fredrik@lindhagen.io>
2021-03-17 11:26:51 +0100

    Validate request header to ensure only valid clients has access

--- ferjepathtaker/main.py
+++ ferjepathtaker/main.py
@@ -1,11 +1,38 @@
 import json
 import os
+import base64
 
 import boto3
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
 ELASTICSEARCH_INDEX_NAME = 'ferry_waypoings'
+
+
+def _get_client_authorizers(headers):
+    authorization = headers.get('Authorization', None)
+
+    if authorization is None:
+        raise ValueError("Authorization header is not present. Please include it in your request")
+
+    http_basic = authorization.replace("Basic", "")
+    if len(http_basic) < 1:
+        raise ValueError("Authorization header is missing 'Basic' prefix. Please provide HTTP basic header")
+
+    http_basic_decoded = base64.b64decode(http_basic.encode('ascii')).decode('ascii')
+    properties = http_basic_decoded.split(':')
+
+    # First in item is client id and second is client secret
+    return properties[0], properties[1]
+
+
+def _require_authorized_client(http_basic: tuple):
+    client_id = os.environ['API_CLIENT_ID']
+    client_secret = os.environ['API_CLIENT_SECRET']
+
+    if client_id != http_basic[0] or client_secret != http_basic[1]:
+        print(f'Client id or client secret is not correct: Client ID: {client_id != http_basic[0]}, Client Secret: {client_secret != http_basic[1]}')
+        raise ValueError('Invalid credentials! Please check they are correctly HTTP basic formated')
 
 
 def _get_es(server) -> Elasticsearch:
@@ -48,6 +75,20 @@ def _get_es(server) -> Elasticsearch:
 
 def handler(event, context):
     print(f'Event: {event}')
+
+    # Ensure only valid requests are included
+    try:
+        authorization = _get_client_authorizers(event['headers'])
+        _require_authorized_client(authorization)
+    except ValueError as err:
+        return {
+            'statusCode': 401,
+            'body': json.dumps({
+                'title': 'Unauthorized',
+                'detail': str(err),
+            }),
+        }
+
     elasticsearch_hostname = os.environ.get("ELASTICSEARCH_HOSTNAME")
     es = _get_es(elasticsearch_hostname)
 
